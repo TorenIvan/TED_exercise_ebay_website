8cf31c1f316b53e9dd5aebdcb63d1b657d7daa8e
TorenIvan <Vaggelisshmos@gmail.com>
2019-07-24 16:23:31 +0300

    sign-up: take input's

--- ebay-new/backend/api/signup.php
+++ ebay-new/backend/api/signup.php
@@ -10,9 +10,27 @@ require 'connect.php';
 $_POST = json_decode(file_get_contents('php://input'), true);
 
 if(isset($_POST) && !empty($_POST)) {
-    //is the user on db
-    // add user to database if he's not
-    //
+
+    //xss protection and of the user of post method(at the same time 2 things)
+    $username = htmlspecialchars($_POST['Username']);
+    $tpassword = htmlspecialchars($_POST['Password']);//temporary password. It is gonna be encoded
+    $name = htmlspecialchars($_POST['Name']);
+    $surname = htmlspecialchars($_POST['Surname']);
+    $email = htmlspecialchars($_POST['Email']);
+    $phone_number = htmlspecialchars($_POST['Phone Number']);
+    $country = htmlspecialchars($_POST['Country']);
+    $state = htmlspecialchars($_POST['State']);
+    $town = htmlspecialchars($_POST['Town']);
+    $address = htmlspecialchars($_POST['Address']);
+    $postcode = htmlspecialchars($_POST['Postcode']);
+    $afm = htmlspecialchars($_POST['TIN / ΑΦΜ']);
+
+    //do not check password maching, we have a master front end dev
+
+    //now we are gonna prepare the first statement to see if there is already a user like him
+    
+
+
     echo json_encode("Let's say that i did that");
 } else {
     // http_response_code("NO ONE REQUESTED THIS! WHY DO YOU ASK FOR IT?!");
