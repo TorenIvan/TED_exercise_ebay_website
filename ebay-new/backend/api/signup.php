2d9c6785ff2fc4982d4feef55280bc5cffff7aba
TorenIvan <Vaggelisshmos@gmail.com>
2019-07-24 18:35:50 +0300

    sign up: done

--- ebay-new/backend/api/signup.php
+++ ebay-new/backend/api/signup.php
@@ -119,6 +119,7 @@ require 'connect.php';
             print_r('2nd try email is ok cause domain is valid!');
           }else {
             print_r('2nd try email is not ok ');
+            json_encode("9");
           }
           //that's all for now m8s
 
@@ -126,6 +127,34 @@ require 'connect.php';
           $salt= substr(str_shuffle(MD5(microtime())), 0, 10);  //optional
           $new_pass = hash('sha384', $param_password);
           print_r($new_pass);
+          print_r("\n");
+
+
+          //  And now let's go to insert the values
+          $sql = "INSERT INTO user (username, password, name, surname, email, phone_number, country, state, town, address, postcode, afm) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+          if($stmt = mysqli_prepare($con, $sql)) {
+            mysqli_stmt_bind_param($stmt, "sssssssssssi", $uusername, $ppassword, $nname, $ssurname, $eemail, $pphone_number, $ccountry, $sstate, $ttown, $aaddress, $ppostcode, $aafm);
+            $uusername=$param_username;
+            $ppassword=$new_pass;
+            $nname=$name;
+            $ssurname=$surname;
+            $eemail=$param_email;
+            $pphone_number=$param_phone_number;
+            $ccountry=$country;
+            $sstate=$state;
+            $ttown=$town;
+            $aaddress=$address;
+            $ppostcode=$postcode;
+            $aafm=$param_afm;
+
+            //execute and insert into the db
+            mysqli_stmt_execute($stmt);
+            print_r("executed\n");
+
+
+          }else {
+            json_encode("10");
+          }
 
         }
 
