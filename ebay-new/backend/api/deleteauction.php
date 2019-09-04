001f86454b8220042d06158c6dd0a79305d5252f
TorenIvan <Vaggelisshmos@gmail.com>
2019-09-04 16:07:05 +0300

    dirs on deleteauction

--- ebay-new/backend/api/deleteauction.php
+++ ebay-new/backend/api/deleteauction.php
@@ -8,9 +8,14 @@ if(isset($_POST) && !empty($_POST)) {
 
   $idd = htmlspecialchars($_POST['id']);
 
+  $sqltlt = "SELECT path from auction WHERE id = $idd;";
+  $resultlt = mysqli_query($con, $sqltlt);
+  $rowltlt = mysql_fetch_assoc($resultlt);
+  $idltlt = $rowltlt['path'];
+
   $sql="DELETE c FROM product_is_category AS c WHERE c.product_id = (SELECT a.product_id FROM product AS p INNER JOIN auction AS a ON p.id=a.product_id WHERE a.id = $idd);";
   $sql1="DELETE a,p FROM product AS p INNER JOIN auction AS a ON p.id=a.product_id WHERE a.id = $idd;" ;
-  
+
   if($result = mysqli_query($con,$sql)){
     if($result1 = mysqli_query($con,$sql1)){
       // print_r("OLA KALA");
@@ -22,7 +27,8 @@ if(isset($_POST) && !empty($_POST)) {
     echo json_encode(-2);
     // print_r("Something is wrong with mysqli_query");
   }
-  
+  require 'deldirs.php';
+
 }else {
   http_response_code("No one requested this!");
 }
