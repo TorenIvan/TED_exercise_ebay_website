c088e0bcb0786b0b588536c7343898cf4361ec5c
TorenIvan <Vaggelisshmos@gmail.com>
2019-09-04 15:46:14 +0300

    dirs on addauction

--- ebay-new/backend/api/dirs.php
+++ ebay-new/backend/api/dirs.php
@@ -1,13 +1,13 @@
 <?php
 
 //$images = "Images";
-$id = 4;
+//$idltlt = 4;
 $e = "/";
 
 $get_path = realpath('../../src/assets').PHP_EOL;
 
 //$folder = $get_path.$e.$images.$id;
-$folder = $get_path.$e.$id;
+$folder = $get_path.$e.$idltlt;
 
 $foldy = preg_replace('/\s/', '', $folder);
 
